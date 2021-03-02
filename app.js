4e8b901aff06c5e98a33e1b6044ec25e5afd471e
Nicke <nikolas.syverod@gmail.com>
2021-03-02 04:50:20 +0200

    added buttons and async function

--- app.js
+++ app.js
@@ -1,16 +1,62 @@
-console.log(fetch("https://api.github.com/users/defunkt"));
+const searchbtn = document.getElementById("search-btn");
+const image = document.getElementById("profile-pic");
+const name1 = document.getElementById("name");
+const userName = document.getElementById("username");
+const website = document.getElementById("website");
+const leftBtn = document.getElementById("left");
+const rightBtn = document.getElementById("right");
 
-async function API() {
-  try {
-    let request = await fetch("https://api.github.com/users/defunkt");
-    let response = await request.json();
-    let userName = response["login"]
-    console.log("json! ", response);
-    console.log(userName)
-    
-  } catch (e) {
-    console.log(e);
-  }
+// The page number the user is currently on
+let pageNumber = 1;
+
+// changes page to next page
+
+rightBtn.addEventListener("click", nextPage);
+
+function nextPage(){
+
+  console.log("rait")
 }
 
-API();
+// changes page to previous page
+leftBtn.addEventListener("click", previousPage);
+
+function previousPage() {
+  console.log("k");
+}
+
+searchbtn.addEventListener("click", getData);
+
+async function getData(e) {
+  try {
+    const inputValue = document.getElementById("input").value;
+    const request = await fetch("https://api.github.com/users/" + inputValue);
+    const response = await request.json();
+
+    // Fetching repos of the current user
+    const reposRequest = await fetch(response["repos_url"]);
+    //returns repos in an array
+    const reposResponse = await reposRequest.json();
+
+    // Check the amount of pages needed for the repos
+    const reposPageAmount = Math.ceil(reposResponse.length / 3)
+
+
+    // Add profile picture
+    image.src = response["avatar_url"];
+
+    // Add name, username and website
+    name1.textContent = response["name"];
+    userName.textContent = response["login"];
+    website.textContent = response["blog"];
+
+    
+    
+    
+    
+    console.log(reposResponse);
+    console.log(response);
+  } catch (e) {
+    console.log("Error! " + e);
+  }
+}
